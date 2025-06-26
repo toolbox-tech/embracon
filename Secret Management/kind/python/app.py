bdb7f62da408700432d4fea55a5662e0f200390d
Marcelo Buzzetti <marcelobuzzetti@toolboxdevops.cloud>
2025-06-26 13:10:35 -0300

    feat(kind): use external secret as env

--- Secret Management/kind/python/app.py
+++ Secret Management/kind/python/app.py
@@ -1,9 +1,12 @@
 from flask import Flask, render_template  # Importe o módulo Flask
+import os # Importe o módulo os para acessar variáveis de ambiente
+segredo = os.environ["SEGREDO_AKV"] # Acesse a variável de ambiente SEGREDO_AKV
 
 app = Flask(__name__)  # Crie uma aplicação Flask
 
 @app.route('/')  # Defina uma rota para a URL raiz ("/")
 def hello_world():  # Defina uma função para lidar com a requisição da URL raiz
+    print(f"Segredo AKV: {segredo}")  # Imprima o segredo AKV no console
     return render_template('index.html')  # Renderiza o template index.html
 
 if __name__ == '__main__':  # Verifique se o script está sendo executado diretamente (não importado)
